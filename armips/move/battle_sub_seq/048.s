231469644e07645e55c1ab2144b3742890915248
Koekenpan <koekenpannosrs@gmail.com>
2024-02-13 23:01:36 +0100

    Make it Rain effect + animation

--- armips/move/battle_sub_seq/048.s
+++ armips/move/battle_sub_seq/048.s
@@ -15,9 +15,18 @@ a001_048:
     checkonsameteam BATTLER_ATTACKER, BATTLER_OPPONENT, _0044
     changevar2 VAR_OP_ADD, VAR_MONEY, VAR_CALCULATION_WORK
 _0044:
-    printmessage 0x332, 0x0, "NaN", "NaN", "NaN", "NaN", "NaN", "NaN"
+    printmessage 0x332, 0x0, "NaN", "NaN", "NaN", "NaN", "NaN", "NaN" // Coins were scattered everywhere!
     waitmessage
     wait 0x1E
+    if IF_EQUAL, VAR_CURRENT_MOVE, MOVE_MAKE_IT_RAIN, _SpAtkDrop
+    endscript
+_SpAtkDrop:
+    changevar2 VAR_OP_SET, VAR_ACTIVE_BATTLER, VAR_ATTACKER
+    changevar VAR_OP_SETMASK, VAR_SERVER_STATUS2, 0x80
+    changevar VAR_OP_SET, VAR_ADD_EFFECT_ATTRIBUTE, SPATK_DOWN
+    gotosubscript 12
+    changevar VAR_OP_CLEARMASK, VAR_SERVER_STATUS2, 0x2
+    changevar VAR_OP_CLEARMASK, VAR_SERVER_STATUS2, 0x80
     endscript
 
 .close
--- armips/move/move_anim/877.s
+++ armips/move/move_anim/877.s
@@ -10,25 +10,36 @@
 .create "build/move/move_anim/0_877", 0
 
 a010_877:
-    loadparticlefromspa 0, 486
+    loadparticlefromspa 0, 37
+    waitparticle
+    loadparticlefromspa 1, 286
     waitparticle
 
-    addparticle 0, 2, 3
-    addparticle 0, 1, 3
-    addparticle 0, 1, 3
-    addparticle 0, 0, 3
-    wait 10
-
-    addparticle 0, 1, 3
-    addparticle 0, 1, 3
-    wait 10
-
-    addparticle 0, 2, 3
-    addparticle 0, 0, 3
-    waitparticle
-
-    unloadparticle 0
+    // "charge up" animation + pay day hit anim on self
+    callfunction 33, 5, 0, 1, 0, 12, 0, "NaN", "NaN", "NaN", "NaN", "NaN"
     waitstate
+    addparticle 1, 0, 3
+    addparticle 1, 1, 3
+    addparticle 1, 2, 3
+    wait 77
+    callfunction 33, 5, 0, 1, 12, 0, 0, "NaN", "NaN", "NaN", "NaN", "NaN"
+    callfunction 42, 8, 258, 100, 110, 100, 80, 100, 327681, 327685, "NaN", "NaN"
+    playsepan 1925, -117
+    waitstate
+    callfunction 42, 8, 258, 100, 100, 100, 120, 100, 1, 327685, "NaN", "NaN"
+    addparticle 0, 1, 3
+    wait 20
+
+    addparticle 0, 0, 17
+    cmd37 6, 0, 2, 6, 1, 0, 0, "NaN", "NaN"
+    playsepanmod 1911, -117, 117, 4, 2
+    wait 10
+    addparticle 0, 1, 4
+    addparticle 0, 3, 4
+    callfunction 36, 5, 1, 0, 1, 2, 264, "NaN", "NaN", "NaN", "NaN", "NaN"
+    playsepan 1906, 117
+    waitparticle
+    unloadparticle 0
     end
     
 
